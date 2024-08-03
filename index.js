f65010b6d2fc1186d9ba38222db2b06e42e8d6f5
Cyzo50218 <155029018+Cyzo50218@users.noreply.github.com>
2024-08-03 12:42:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -123,18 +123,22 @@ if (!grammarRules.length)  {
   "name": "Attach First KP Sound in Prefixes",
   "description": "Ito ay inuulitang unang katinig at patinig (KP) ng salita.",
   "pattern": [
-    { "regex": "\\b(mag)([bcdfghjklmnpqrstvwxyz][aeiou]\\w*)\\b" },
-    { "regex": "\\b(Mag)([bcdfghjklmnpqrstvwxyz][aeiou]\\w*)\\b" },
-    { "regex": "\\b(mg)([bcdfghjklmnpqrstvwxyz][aeiou]\\w*)\\b" },
-    { "regex": "\\b(Mg)([bcdfghjklmnpqrstvwxyz][aeiou]\\w*)\\b" }
+    { "regex": "\\b(mag)\\s*([bcdfghjklmnpqrstvwxyz][aeiou]\\w*)\\b" },
+    { "regex": "\\b(Mag)\\s*([bcdfghjklmnpqrstvwxyz][aeiou]\\w*)\\b" },
+    { "regex": "\\b(mg)\\s*([bcdfghjklmnpqrstvwxyz][aeiou]\\w*)\\b" },
+    { "regex": "\\b(Mg)\\s*([bcdfghjklmnpqrstvwxyz][aeiou]\\w*)\\b" },
+    { "regex": "\\b(magpo|Magpo|mgpo|Mgpo)\\s*([bcdfghjklmnpqrstvwxyz][aeiou]\\w*)\\b" }
   ],
   "message": "Ito ay inuulitang unang katinig at patinig (KP) ng salita.",
   "suggestions": [
-    { "text": "$1$2-$2" }
+    { "text": "mag$2{0,2}-$2" },
+    { "text": "mag-$2" }
   ],
   "examples": [
     { "incorrect": "magcomputer", "correct": "magco-computer" },
-    { "incorrect": "magphotocopy", "correct": "magpo-photocopy" }
+    { "incorrect": "magphotocopy", "correct": "magpo-photocopy" },
+    { "incorrect": "mag photocopy", "correct": "magpo-photocopy" },
+    { "incorrect": "magpophotocopy", "correct": "magpo-photocopy" }
   ]
 },
 {
