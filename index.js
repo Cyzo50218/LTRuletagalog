cd0d99c99ea7daf68568269a2241b588c5872285
Cyzo50218 <155029018+Cyzo50218@users.noreply.github.com>
2024-08-21 16:59:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1528,6 +1528,7 @@ const checkTextAgainstRules = async (text, rules) => {
         while ((repeatedMatch = repeatedWordRegex.exec(text)) !== null) {
           if (repeatedMatch.index >= match.index && repeatedMatch.index < (match.index + match[0].length)) {
             // Adjust suggestions for repeated words
+            const repeatedSuggestions = [`Consider removing the repeated word: "${repeatedMatch[0]}"`];
             
             // Add repeated word match to results
             matches.push({
