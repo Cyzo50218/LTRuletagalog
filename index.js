ae81ed587d804bcac1bd28c5f04fdb78e43767b7
Cyzo50218 <myfavoritemappingswar@gmail.com>
2024-11-02 08:34:40 +0800

    update index.js

--- index.js
+++ index.js
@@ -3056,22 +3056,6 @@ if (!grammarRules.length)  {
   ]
 },
 {
-  "id": "GITLING_1_1",
-  "name": "Add Gitling - abalang abala",
-  "pattern": [
-    {
-      "regex": "\\b(ibat)(-)(ibang)"
-    }
-  ],
-  "message": "maglagay ng gitling",
-  "description": "ang salitang iba't iba ay pinaikli mula sa iba at iba, kaya hindi kinakailangan ng isa pang gitling sa pagitan ng iba't iba",
-  "suggestions": [
-    {
-      "text": "$1 $3"
-    }
-  ]
-},
-{
       "id": "GITLING_1",
       "name": "Add Gitling - abalang abala",
       "pattern": [
@@ -3125,6 +3109,22 @@ if (!grammarRules.length)  {
   "pattern": [
     {
       "regex": "(\\b([Ii]sa))(t)(-)(\\b(isang))"
+}
+      ],
+  "message": "maglagay ng gitling",
+  "description": "ang salitang iba't iba ay pinaikli mula sa iba at iba, kaya hindi kinakailangan ng isa pang gitling sa pagitan ng iba't iba",
+  "suggestions": [
+    {
+      "text": "$1'$3 $5"
+        }
+      ]
+},
+{
+  "id": "GITLING_1",
+  "name": "Add Gitling - abalang abala",
+  "pattern": [
+    {
+      "regex": "(\\b([Ii]ba))(t)(-)(\\b(ibang))"
 }
       ],
   "message": "maglagay ng gitling",
